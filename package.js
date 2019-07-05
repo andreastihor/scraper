0c7f0241c11d8eefaf091a94259ece2ae40ec445
Andreas <t-andreas.tihor@ovo.id>
2019-07-05 14:38:07 +0700

    added pptr

--- package.js
+++ package.js
@@ -14,10 +14,36 @@ request = request.defaults({
 const fs = require('fs')
 const get = Promise.promisify(request.get)
 const post = Promise.promisify(request.post)
+const puppeteer = require('puppeteer')
+async function launchBrowser() {
+  const browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 50,
+    args: [
+      '--no-sandbox',
+      '--disable-web-security',
+      '--disable-features=site-per-process',
+      '--ignore-certificate-errors',
+      '--incognito',
+    ]
+  },);
+  const pages = await browser.pages();
+  const page = pages[0];
+  await page.setViewport({
+    width: 1400,
+    height: 1000,
+  });
+  page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36');
+  return {
+    page,
+    browser,
+  };
+}
 module.exports = {
   get,
   post,
   cheerio,
   fs,
-  
+  request,
+  launchBrowser,
 }
